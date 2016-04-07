91be8c4c0db147c172572325d017ce7fa340d9c9
Naren Ragav [n22] <n22@anode.ornl.gov>
2016-04-07 16:02:25 -0400

    Linked cpu count between wrap and sweep. Changed cwd of xyzdtp_gen.out binary to be called dynamically within current simulation folder

--- scripts/wrap.py
+++ scripts/wrap.py
@@ -4,36 +4,36 @@ import optparse
 from puqutil import dump_hdf5
 from subprocess import call
 
-# you want PUQ to decide that
-cpus = 32
-
-usage = "usage: %prog --on_time x --power y --wait_time z"
+usage = "usage: %prog --np cpus --on_time x --power y --wait_time z" 
 parser = optparse.OptionParser(usage)
+parser.add_option("--np", type=int)
 parser.add_option("--on_time", type=float)
 parser.add_option("--power", type=float)
 parser.add_option("--wait_time", type=float)
 (options, args) = parser.parse_args()
 
 # get the parameters from PUQ
+cpus = options.np
 x = options.on_time
 y = 200 #options.power
 z = 10 #options.wait_time
 
 # generate input path file
-PUQ_dir = '/home/n22/IN718/PUQ_test'
-scripts_dir = PUQ_dir + '/scripts'
-launch_dir = PUQ_dir + '/5_spot'
-result_dir = PUQ_dir + '/5_spot/test_output' 
+PUQ_dir = '/home/n22/IN718/PUQ_test/'
+case_folder = '5_spot'
+scripts_dir = PUQ_dir + 'scripts'
+launch_dir = PUQ_dir + case_folder
+result_dir = PUQ_dir + case_folder +'/test_output' 
 cmd = ['./xyzdtp_gen.out', str(x), str(y), str(z)]
-call(cmd, cwd=scripts_dir)
+call(cmd, cwd=os.getcwd())
 
 # copy the input path file
-cmd =['cp', 'xyzdtp.dat', '../5_spot/']
-call(cmd, cwd=scripts_dir)
+#cmd =['cp', 'xyzdtp.dat', '../'+ case_folder +'/']
+#call(cmd, cwd=scripts_dir)
 
 # launch the simulation
 truchas_binary = '/home/srdjans/projects/truchas-2.8.0-RC/install/linux.x86_64.intel.parallel.opt/bin/t-linux.x86_64.intel.parallel.opt-2.8.0'
-cmd = ['nohup', 'mpirun', '-np', str(cpus), truchas_binary, 'test.inp']
+cmd = ['mpirun', '-np', str(cpus), truchas_binary, 'test.inp']
 call(cmd, cwd=launch_dir)
 
 # postprocess the results

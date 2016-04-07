51e3238b38f33ef950408fdeb88f55e366d191fe
Naren Ragav [n22] <n22@anode.ornl.gov>
2016-04-07 16:03:08 -0400

    Linked cpu count between wrap and sweep

--- scripts/sweep.py
+++ scripts/sweep.py
@@ -3,7 +3,12 @@
 from puq import UniformParameter, InteractiveHost, Smolyak, TestProgram, Sweep
 
 PUQ_dir = '/home/n22/IN718/PUQ_test/'
+case_folder = '5_spot'
 def run():
+    # setup
+    cpus = 24
+    cpus_per_node = 48
+
     # declare your parameters
     parameters = [
         UniformParameter('x', 'on time [s]', min=0.1e-3, max=2e-3)
@@ -13,15 +18,15 @@ def run():
         #]
     
     # create an host (interactive or PBS)
-    host = InteractiveHost(cpus=32, cpus_per_node=32)
+    host = InteractiveHost(cpus=cpus, cpus_per_node=cpus_per_node)
     
     # choose a UQ method
     algo = Smolyak(parameters, level=1)
     
-    prog = TestProgram(exe=PUQ_dir+'scripts/wrap.py --on_time=$x',# --power=$y --wait_time=$z',
-            desc='Trial test with two parameters',
-            newdir=False)
-            #infiles =[PUQ_dir+'test/block.exo',PUQ_dir+'test/test.inp',PUQ_dir+'test/xyzdtp.dat'])
+    prog = TestProgram(exe=PUQ_dir+'scripts/wrap.py --np='+str(cpus)+' --on_time=$x',# --power=$y --wait_time=$z',
+            desc='Trial test for parallel runs',
+            newdir=True,
+            infiles =[PUQ_dir+case_folder+'/block.exo',PUQ_dir+case_folder+'/test.inp',PUQ_dir+'scripts/xyzdtp_gen.out'])
 
     return Sweep(algo, host, prog)
 
